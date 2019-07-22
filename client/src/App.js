c529626d8b092198c0a4c1d32c631045105fa40e
d3non <peps.valle@gmail.com>
2019-07-22 16:42:21 -0500

    Updating functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,26 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import SaveBooks from "./pages/SaveBooks";
-import SearchBooks from "./pages/SearchBooks";
-import NoMatch from "./pages/NoMatch";
+import React, { Component } from "react";
+import "./App.css";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 import Nav from "./components/Nav";
-import Footer from "./components/Footer"
-import "./App.css"
+import Header from "./components/Header";
 
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={SearchBooks} />
-          <Route exact path="/saved" component={SaveBooks} />
-          <Route exact path="/saved/:id" component={SaveBooks} />
-          <Route component={NoMatch} /> 
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Search}/>
+            <Route path="/saved" component={Saved}/>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,29 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 
 export default {
-    // Get book from google search 
-    getGoogleSearchBooks: function(query) {
-        return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query)
-    },
-
-    // Gets all books
-    getBooks: function () {
+    getBooks: () => {
         return axios.get("/api/books");
     },
-
-    // Gets the book with the given id
-    getBook: function (id) {
-        return axios.get("/api/books/" + id);
+    searchBooks: (title) => {
+        return axios.post("/search", {title: title});
     },
-
-    // Saves a book to the database
-    saveBook: function (savedBooks) {
-        return axios.post("/api/books", savedBooks);
+    addBookToDB: (bookData) => {
+        return axios.post("/api/books", bookData);
     },
-
-    // Deletes the book with the given id
-    deleteBook: function (id) {
-        return axios.delete("/api/books/" + id);
+    deleteBook: (id) => {
+        return axios.delete(`/api/books/${id}`);
     }
-
 }
