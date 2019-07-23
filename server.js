e73ed6871fed9635653da302de8108215f1fca29
d3non <peps.valle@gmail.com>
2019-07-23 14:34:39 -0500

    Changing mongodb connection

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
-const mongoose = require("mongoose");
+/*const mongoose = require("mongoose");
 //const mongoURL = process.env.PROD_MONGODB || "mongodb://localhost:27017/googlebooks"
 const mongoURL = process.env.PROD_MONGODB || "mongodb://localhost/googlebooks";
 
@@ -21,7 +21,37 @@ mongoose.connect(mongoURL, {useNewUrlParser: true})
   })
   .catch((err) => {
     console.log(`Error connecting to mongoDB error: ${err}`);
-  });
+  });*/
+
+// configure mongoose and start the server
+// =============================================================
+// set mongoose to leverage promises
+mongoose.Promise = Promise;
+mongoose.set('useCreateIndex', true)
+
+//const dbURI = process.env.MONGODB_URI || "mongodb://localhost:27017/newsArticles";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+mongoose.connect(MONGODB_URI);
+
+// Database configuration with mongoose
+
+//mongoose.connect(dbURI, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+    console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+    console.log("Mongoose connection successful.");
+    // start the server, listen on port 3000
+    app.listen(PORT, function() {
+        console.log("App running on port " + PORT);
+    });
+});
 
 require("./routes/api-routes")(app);
 
